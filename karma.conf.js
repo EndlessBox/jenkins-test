56e5e959c69aec8377681151e67ba7cbec262ef7
EndlessBox <younes.bouladhane.dev@gmail.com>
2020-06-11 13:09:00 +0100

    add karma-junit and chromeheadless

--- karma.conf.js
+++ karma.conf.js
@@ -3,32 +3,47 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-     // require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-junit-reporter"),
+      // require('karma-phantomjs-launcher'),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/test'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require("path").join(__dirname, "./coverage/test"),
+      reports: ["html", "lcovonly", "cobertura", "text-summary"],
+      "report-config": {
+        cobertura: {
+          file: "cobertura.xml",
+        },
+      },
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml", "junit"],
+    junitReporter: {
+      outputDir: "test-results",
+      outputFile: "unit-test-results.xml",
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    //browsers: ['Chrome'],
-    browsers: ['PhantomJS'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
+    browsers: ["ChromeHeadlessNoSandbox" /*, 'Chrome'*/],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
